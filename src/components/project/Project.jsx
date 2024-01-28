67411a5c931bf63eca24bd510909b4e6ccc31f0e
Sajal <shrivastavas1510@gmai.com>
2024-01-29 01:57:42 +0530

    git add -A

--- src/components/project/Project.jsx
+++ src/components/project/Project.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import "./project.css";
 import IMG1 from "../../assests/project1.png";
 import NoteIMG from "../../assests/NoteApp.png";
-import SocialIMG from "../../assests/SocialAppDark.png";
+import SocialIMG from "../../assests/hostel.png";
 import FilmIMG from "../../assests/FilmFliker.png";
 import IMG2 from "../../assests/project2.png";
 const Portfolio = () => {
@@ -16,13 +16,13 @@ const Portfolio = () => {
           <div className="portfolio__item-image">
             <img src={SocialIMG} alt="" />
           </div>
-          <h3>SocialCircle - Social Media Web-App</h3>
+          <h3>HOSTELENGINE.COM - Hostel Booking Website</h3>
           <div className="portfolio__item-cta">
-            <a href="https://github.com/ManishK4514/SocialCircle" className="btn" target="_blank">
+            <a href="https://github.com/sazzisgithub/Hostel_Booking_Website.git" className="btn" target="_blank">
               Github
             </a>
             <a
-              href="http://socialcircle.vercel.app/"
+              href="https://sazzisgithub.github.io/Hostel_Booking_Website/"
               className="btn btn-primary"
               target="_blank"
             >
